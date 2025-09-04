ba7aa987d11c0ab3b933681ea510914961a74e3b
ethanhunt7089 <ethanhunt7089@gmail.com>
2025-09-04 21:38:22 +0700

    query: fix react-query

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -13,14 +13,9 @@ export default function QueryProvider({ children }: QueryProviderProps) {
         defaultOptions: {
           queries: {
             staleTime: 5 * 60 * 1000, // 5 นาที
-            cacheTime: 10 * 60 * 1000, // 10 นาที
+            gcTime: 10 * 60 * 1000, // 10 นาที
             retry: 1,
             refetchOnWindowFocus: false,
-          },
-          infiniteQueries: {
-            staleTime: 5 * 60 * 1000,
-            cacheTime: 10 * 60 * 1000,
-            maxPages: 3, // เก็บแค่ 3 หน้าล่าสุด
           },
         },
       })
